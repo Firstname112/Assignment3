9eb39db08e8ceefbd496fb768437d6f37e8abe18
GreenGum <virelekana12@gmail.com>
2026-04-27 12:21:47 +0700

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
-#include <string>
-
-#include "include/orang.hpp"
-#include "include/dosen.hpp"
-#include "include/mahasiswa.hpp"
-#include "include/tendik.hpp"
+#include "include/Mahasiswa.hpp"
+#include "include/MataKuliah.hpp"
+#include "include/KRS.hpp"
 
 using namespace std;
-using namespace sim;
 
-int main(int argc, char ** argv)
-{
-    
+int main() {
+    // Input data mahasiswa
+    string nama, nim;
+    cout << "Masukkan Nama Mahasiswa: ";
+    getline(cin, nama);
+    cout << "Masukkan NIM: ";
+    getline(cin, nim);
+
+    Mahasiswa mhs(nama, nim);
+
+    // Input data mata kuliah
+    string namaMK;
+    int sks;
+    cout << "\nMasukkan Nama Mata Kuliah: ";
+    getline(cin, namaMK);
+    cout << "Masukkan SKS: ";
+    cin >> sks;
+
+    MataKuliah mk(namaMK, sks);
+
+    // Membuat KRS
+    KRS krs(mhs, mk);
+
+    // Menampilkan data
+    cout << "\n=== Data KRS ===" << endl;
+    krs.tampilkan();
+
+    // Supaya tidak langsung tertutup
+    cin.ignore();
+    cin.get();
 
     return 0;
 }
